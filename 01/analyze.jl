cc41612519212b47040a07458991efd756846918
matthias <b1d@mailbox.org>
2021-12-01 21:56:56 +0100

    Add solution for second star

--- 01/analyze.jl
+++ 01/analyze.jl
@@ -12,3 +12,14 @@ for idx in 2:size(df)[1]
 end
 
 println("Values in df are ", count, " times increasing.")
+
+# Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+
+count_2 = 0
+for idx in 3:(size(df)[1]-1)
+    if sum(df[(idx-1):(idx+1), :Column1]) - sum(df[(idx-2):idx, :Column1]) > 0
+        count_2 += 1
+    end
+end
+
+println("Rolling-three-sum is ", count_2, " times increasing.")
