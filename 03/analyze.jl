c288aad943c291ec47012dfd39b83de7c93c9ab2
matthias <b1d@mailbox.org>
2021-12-03 18:26:38 +0100

    Add solution for star 2

--- 03/analyze.jl
+++ 03/analyze.jl
@@ -7,8 +7,7 @@ df = CSV.read("03/input.txt", DataFrame; header = false, types=String)
 result_mat = ones(0, 12)
 for idx in 1:size(df)[1]
     row_vec = split(df[idx, :Column1], "")
-    convert(Int, row_vec)
-    result_mat = [result_mat;permutedims(row_vec)] # WTF: https://discourse.julialang.org/t/transpose-of-an-array-of-strings/40431/3
+    result_mat = [result_mat; permutedims(row_vec)] # WTF: https://discourse.julialang.org/t/transpose-of-an-array-of-strings/40431/3
 end
 
 result_mat = parse.(Int, result_mat)
@@ -28,3 +27,44 @@ end
 # Solution for star 1:
 print( Int(0b110111000111) * Int(0b001000111000))
 
+# https://adventofcode.com/2021/day/3#part2
+# What is the life support rating of the submarine?
+
+# oxygen_generator_rating
+iter_mat = copy(result_mat)
+for act_col in 1:12
+    col_sum = sum(iter_mat[:, act_col])
+    number_of_rows = size(iter_mat)[1]
+    if col_sum > number_of_rows/2
+        most_column_value = 1
+    elseif col_sum == number_of_rows/2
+        most_column_value = 1
+    else
+        most_column_value = 0
+    end
+    iter_mat = iter_mat[iter_mat[:, act_col] .== most_column_value, :]
+end
+
+iter_mat
+# --> 100111110011
+
+# co2_scrubber_rating (just reverse the rules)
+iter_mat = copy(result_mat)
+for act_col in 1:12
+    number_of_rows = size(iter_mat)[1]
+    if number_of_rows > 1
+        col_sum = sum(iter_mat[:, act_col])
+        if col_sum > number_of_rows/2
+            least_column_value = 0
+        elseif col_sum == number_of_rows/2
+            least_column_value = 0
+        else
+            least_column_value = 1
+        end
+        iter_mat = iter_mat[iter_mat[:, act_col] .== least_column_value, :]
+    end
+end
+
+# --> 001011100001
+
+print(Int(0b100111110011) * Int(0b001011100001))
